b5e7d59c77e891db24df42070c71c54624d9f5bd
Trevor-ZBook\Trevor <Trevor96@tamu.edu>
2018-09-14 00:49:04 -0500

    [Added] - game directior and basic framework

--- 487_SemesterProject_UnityProject/Assets/Scripts/GameDirector.cs
+++ 487_SemesterProject_UnityProject/Assets/Scripts/GameDirector.cs
@@ -5,6 +5,48 @@ using UnityEngine;
 public class GameDirector : Singleton<GameDirector>
 {
 
+    public bool isMaxStressed = false;
+    public float timeMaxStressed = 30f;
+    public int stressCurrent = 0;
+    public int stressMax = 10000;
 
+    public void AddStress(int amount)
+    {
+        if (isMaxStressed)
+        {
+            return;
+        }
+        stressCurrent += Mathf.Abs(amount);
+        CheckStress();
+    }
 
+    public void RemoveStress(int amount)
+    {
+        if (isMaxStressed)
+        {
+            return;
+        }
+        stressCurrent -= Mathf.Abs(amount);
+        CheckStress();
+    }
+
+    void CheckStress()
+    {
+        if (stressCurrent < 0)
+        {
+            stressCurrent = 0;
+        }
+        if (stressCurrent >= stressMax)
+        {
+            StartCoroutine(HoldMaxStress());
+        }
+    }
+
+    IEnumerator HoldMaxStress()
+    {
+        isMaxStressed = true;
+        yield return new WaitForSeconds(timeMaxStressed);
+        isMaxStressed = false;
+        stressCurrent = 0;
+    }
 }

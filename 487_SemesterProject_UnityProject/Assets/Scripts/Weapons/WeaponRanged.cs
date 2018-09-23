2ac2ef287cecaa784a9d7a4bda7b8ef2512bce03
Nick Muniz <nickam521@gmail.com>
2018-09-23 02:56:43 -0500

    [Added] - basic ranged weapon implementation

---
+++ 487_SemesterProject_UnityProject/Assets/Scripts/Weapons/WeaponRanged.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRanged : Weapon
+{
+    public Projectile projectilePrefab;
+
+    public float projectileSpeed = 1f;
+    public float projectileDestroyTime = 3f;
+    public LayerMask layerMaskToShoot;
+
+    protected override void Attack()
+    {
+        base.Attack();
+        Projectile spawnedProjectile = ObjectPoolingManager.instance.CreateObject(projectilePrefab, null, projectileDestroyTime) as Projectile;
+        spawnedProjectile.transform.position = transform.position;
+        spawnedProjectile.ShootProjectile(transform.forward, projectileSpeed, 1);
+    }
+}

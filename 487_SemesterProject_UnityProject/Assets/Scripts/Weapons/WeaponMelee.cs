457ffb03074181b39391e62c285c70cdb37ab2db
Nick Muniz <nickam521@gmail.com>
2018-10-01 03:59:00 -0500

    [Added] - crappy rotation for melee weapons, added test melee weapon, added enum for different melee types

--- 487_SemesterProject_UnityProject/Assets/Scripts/Weapons/WeaponMelee.cs
+++ 487_SemesterProject_UnityProject/Assets/Scripts/Weapons/WeaponMelee.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class WeaponMelee : Weapon
 {
     public Transform hitbox;
+    public Transform startRotation;
+    public Transform endRotation;
+    public Transform rotationAxis;
 
     protected override void Attack()
     {
         base.Attack();
+        StartCoroutine(AttackCoroutine());
+    }
 
+    public IEnumerator AttackCoroutine()
+    {
+        float nextAttack = 0f;
+        while (nextAttack < speedAttack)
+        {
+            nextAttack += Time.deltaTime;
+            hitbox.rotation = Quaternion.Lerp(startRotation.rotation, endRotation.rotation, (nextAttack / speedAttack));
+            yield return null;
+        }
+        
+        hitbox.position.Set(0, 0, 0);
+        hitbox.rotation = startRotation.rotation;
     }
 }

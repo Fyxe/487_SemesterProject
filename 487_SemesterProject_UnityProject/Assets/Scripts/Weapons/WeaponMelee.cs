328382335bce444150bc65122a15bf79380e7f49
Nick Muniz <nickam521@gmail.com>
2018-10-24 00:53:48 -0500

    [Fixed] - melee weapon now damages [Bug] - Transitioning to level replaces melee weapon with base pistol [Bug] - Enemies no longer start following immediately

--- 487_SemesterProject_UnityProject/Assets/Scripts/Weapons/WeaponMelee.cs
+++ 487_SemesterProject_UnityProject/Assets/Scripts/Weapons/WeaponMelee.cs
@@ -31,15 +31,33 @@ public class WeaponMelee : Weapon
     {
         rotationToResetTo = transform.rotation;
         float currentTime = 0f;
+        List<Collider> enemiesHit = new List<Collider>();
+
         while (currentTime < speedAttack)
         {
             currentTime += Time.deltaTime;
+            
+            RaycastHit m_Hit;
+
             hitbox.rotation = Quaternion.Slerp(startRotation.rotation, endRotation.rotation, (currentTime / speedAttack));
+            if (Physics.BoxCast(GetComponentInChildren<Collider>().bounds.center, transform.localScale, transform.forward, out m_Hit, transform.rotation, rangeAttack))
+            {
+                //Output the name of the Collider your Box hit
+                Debug.Log("Hit : " + m_Hit.collider.name + " is damageable?: " + (m_Hit.collider.gameObject.GetComponentInParent<Damageable>() != null) + " has enemy tag?: " + (m_Hit.collider.gameObject.tag == "Enemy") + " has been hit before?: " + enemiesHit.Contains(m_Hit.collider));
+                if (m_Hit.collider.gameObject.GetComponentInParent<Damageable>() != null && m_Hit.collider.gameObject.tag == "Enemy" && !enemiesHit.Contains(m_Hit.collider))
+                {
+                    m_Hit.collider.gameObject.GetComponentInParent<Damageable>().HurtToDeath();
+                    enemiesHit.Add(m_Hit.collider);
+                }
+            }
+
+
             yield return null;
         }
         
         hitbox.position.Set(0, 0, 0);
         hitbox.rotation = rotationToResetTo;
+        enemiesHit.Clear();
     }
 
     public IEnumerator AttackCoroutineStab()
